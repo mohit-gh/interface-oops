1697359a7f588f847e8eac5380cd7b37efba1c9f
mohit-gh <mohitjajani85@gmail.com>
2018-05-03 17:44:53 +0530

    Update errors.php

--- errors.php
+++ errors.php
@@ -26,5 +26,42 @@
 
   echo $c->toJson();
 //Fatal error : Class Collection contains 1 abstract method and must therefore be declared abstract or implement the remaining
-methods (Countable::count)
+                //methods (Countable::count)
 ?>
+-----------------------------------------------------------------------------------------------------------------------------------
+<?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+
+  class Collection implements Countable, JsonSerializable {
+
+    protected $items = [];
+
+    public function add($value) {
+      $this->items[] = $value;
+    }
+
+    public function set($key, $value) {
+      $this->items[$key] = $value;
+    }
+
+    public function toJson() {
+      return json_encode($this->items);
+    }
+
+    public function count() {
+      return  count($this->items);
+    }
+
+  }
+
+  $c = new Collection();
+  $c->add('Mohit');
+  $c->add('Mayur');
+
+  echo $c->toJson()."<br>";
+  echo count($c);
+
+?>
+//Fatal error : Class Collection contains 1 abstract method and must therefore be declared abstract or implement the remaining
+methods (JsonSerializable::jsonSerialize)
